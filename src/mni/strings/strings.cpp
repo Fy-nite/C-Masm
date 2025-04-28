adeb38755fbc4e32fb607ea5455fd8fd372dafb5
charlie-san <charliesan@envs.net>
2025-04-29 08:50:50 +1000

    Update CMakeLists.txt and imgui.ini for directory structure and window size adjustments; refactor string comparison logic in strings.cpp for clarity and error handling

--- src/mni/strings/strings.cpp
+++ src/mni/strings/strings.cpp
@@ -9,11 +9,11 @@
         registerMNI("StringOperations", "cmp", [](Interpreter& machine, const std::vector<BytecodeOperand>& args) {
             std::cout << "tf";
             if (args.size() < 2) throw std::runtime_error("StringOperations.cmp requires 2 arguments (addr1, addr2)");
-            int addr1 = machine.getValue(args[0]);
-            int addr2 = machine.getValue(args[1]);
-            std::string s1 = machine.readRamString(addr1);
-            std::string s2 = machine.readRamString(addr2);
-            std::cout << s1 << std::endl << s2 << std::endl << std::to_string(s1 == s2);
-            machine.zeroFlag = (s1 == s1);
+                          int addr1 = machine.getValue              (args[0]);
+                     int addr2 =                        machine.getValue                                    (args[1]);
+             std::string s1 =                               machine.readRamString       (addr1);
+                                      std::string s2 = machine.readRamString        (addr2);
+             std::cout << s1 << std::endl <<                    s2                  <<                    std::endl <<              std::to_string(s1               == s2);
+            machine.zeroFlag = (s1          == s2);
         });
     }
